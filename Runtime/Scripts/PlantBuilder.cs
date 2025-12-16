826f7ae3e53329a14d7e9f076daa2bb944ae8f4e
Luzzi <95920754+LucasLEONARDUZZI@users.noreply.github.com>
2025-12-17 00:50:50 +0100

    Add initial PlantBuilder runtime scripts and shaders
    Introduces core runtime scripts for plant building, including HelixModifier, MeshCombiner, MeshSaver, PlantBuilder, PlantBuilderEditor, PlantHierarchy, and PlantNode. Adds compute shader for helix modification, shader graphs, utility shader subgraphs, and sample assets for demonstration. This is the initial commit for the PlantBuilder system.

---
+++ Runtime/Scripts/PlantBuilder.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Luzzi.PlantBuilder{
+[ExecuteInEditMode, RequireComponent(typeof(MeshCombiner))]
+public class PlantBuilder : PlantHierarchy
+{
+    private bool _editMode = false;
+    public bool EditMode => _editMode;
+
+    private bool _applyModifiers = false;
+    public bool ApplyModifiers => _applyModifiers;
+
+    [SerializeField, Range(0f, 1f), HideInInspector]
+    private float _lifeCycleFactor = 1f;
+
+    [SerializeField]
+    private float _autoRefreshRate = 1f;
+    private float _timer;
+
+    [SerializeField]
+    private ComputeShader _helixModifier;
+
+    private MeshCombiner _combiner => GetComponent<MeshCombiner>();
+
+    public void SetEditMode(bool enable)
+    {
+        _editMode = enable;
+        if (enable)
+        {
+            _combiner.UnCombine();
+        }
+        else
+        {
+            _combiner.Combine(GetObjetsMeshesToCombine());
+            // Normalize UV2.w (remap all values between 0 and 1) as it is used as growth progression in shader
+            _combiner.NormalizeUVs(2, 3);
+            // Set UV1.z as a constant to be used in shader as the lifeCycleSpeedFactor
+            Debug.Log("_lifeCycleSpeedFactor " + _lifeCycleFactor);
+            _combiner.SetUVsConstant(1, 3, _lifeCycleFactor);
+            _combiner.SaveMesh();
+        }
+
+        _applyModifiers = false;
+        Refresh(true);
+    }
+
+    public void SetApplyModifiers(bool enable)
+    {
+        if (_applyModifiers != enable)
+        {
+            _applyModifiers = enable;
+            Refresh(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (_editMode)
+        {
+            _timer += Time.deltaTime;
+            float duration = 1f / Mathf.Max(_autoRefreshRate, float.Epsilon);
+            if (_timer >= duration)
+            {
+                _timer = 0f;
+                Refresh();
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        Refresh();
+    }
+
+    public void Refresh(bool refreshModifiers = false)
+    {
+        // Only in edit mode
+        if (!_editMode) return;
+        if (!Application.isEditor) return;
+
+        var self = this;
+        EditorApplication.delayCall += () =>
+        {
+            if (self == null) return;
+
+            self.UpdateHierarchy(self, self._helixModifier);
+            for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+            {
+                if (self._nodes[nodeIndex] == null) continue;
+                self._nodes[nodeIndex].UpdateMeshes();
+            }
+
+            if (refreshModifiers)
+            {
+                for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+                {
+                    if (self._nodes[nodeIndex] == null) continue;
+                    self._nodes[nodeIndex].SetEnableModifier(self._applyModifiers);
+                }
+            }
+        };
+    }
+
+    private Dictionary<GameObject, Mesh> GetObjetsMeshesToCombine()
+    {
+        PlantNode[] nodes = GetComponentsInChildren<PlantNode>();
+        Dictionary<GameObject, Mesh> objectsMeshes = new Dictionary<GameObject, Mesh>();
+        for (int index = 0; index < nodes.Length; index++)
+        {
+            PlantNode node = nodes[index];
+            objectsMeshes.Add(node.gameObject, node.ModifiedMesh);
+        }
+
+        return objectsMeshes;
+    }
+}
+}
---
+++ Runtime/Scripts/PlantNode.cs
@@ -0,0 +1,288 @@
+using UnityEditor;
+using UnityEngine;
+namespace Luzzi.PlantBuilder{
+[ExecuteInEditMode, RequireComponent(typeof(MeshFilter))]
+public class PlantNode : PlantHierarchy
+{
+    private bool _canBeModified;
+
+    [SerializeField]
+    private bool _useRadialGrowth = true;
+
+    [SerializeField]
+    private HelixSettings _modifierSettings = new HelixSettings(new Vector2(0.1f, 0.1f), 1f, 0f, 0f);
+
+    private MeshFilter _meshFilter => GetComponent<MeshFilter>();
+    private ComputeShader _computeShader;
+    private HelixModifier _modifier;
+    public Mesh _baseMesh;
+    public Mesh _modifiedMesh;
+    public Mesh ModifiedMesh => _modifiedMesh;
+
+    public void Setup(ComputeShader computeShader)
+    {
+        if (_computeShader == null)
+        {
+            _computeShader = computeShader;
+        }
+    }
+    private void OnValidate()
+    {
+        Refresh();
+    }
+
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        // avoid send message warning by making a delay call
+        var self = this;
+        EditorApplication.delayCall += () =>
+        {
+            if (self == null) return;
+            if (Application.isEditor)
+            {
+                self.UpdateModifier();
+                self.GenerateUV_LocalXY(3);
+            }
+        };
+    }
+
+    public void UpdateModifier()
+    {
+        if (_canBeModified)
+        {
+            ApplyHelixModifier();
+        }
+    }
+
+    public void SetEnableModifier(bool enable)
+    {
+        if (_canBeModified != enable)
+        {
+            _canBeModified = enable;
+            if (enable)
+            {
+                ApplyHelixModifier();
+            }
+            else
+            {
+                RevertHelixModifier();
+            }
+
+            for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+            {
+                if (_nodes[nodeIndex] == null) continue;
+                _nodes[nodeIndex].SetEnableModifier(enable);
+            }
+        }
+
+    }
+
+    private void ApplyHelixModifier()
+    {
+        // Mesh modifier
+        if (_modifier == null)
+        {
+            _modifier = new HelixModifier(_computeShader);
+        }
+
+        if (_baseMesh == null || _modifiedMesh == null)
+        {
+            return;
+        }
+
+        _modifier.Compute(_baseMesh, _modifierSettings, ref _modifiedMesh, out Vector3[] curveCenters);
+        GenerateUV_LocalYMask(2, in curveCenters);
+        _meshFilter.mesh = _modifiedMesh;
+
+        for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+        {
+            if (_nodes[nodeIndex] == null) continue;
+            Vector3 baseLocalPosition = new Vector3(0f, _nodes[nodeIndex].gameObject.transform.localPosition.y, 0f);
+            float height = _baseMesh.bounds.extents.y * 2;
+            Vector3 newLocalPosition = _modifier.HelixCurve(baseLocalPosition, height, _modifierSettings);
+            _nodes[nodeIndex].gameObject.transform.localPosition = newLocalPosition;
+        }
+    }
+
+    public void UpdateMeshes()
+    {
+        bool udpated = false;
+        //string baseMeshName = gameObject.name + "_basemesh";
+        //if (_baseMesh == null) || _baseMesh.name != baseMeshName)
+        //{
+        if (_baseMesh == null && !_canBeModified)
+        {
+            _baseMesh = _meshFilter.sharedMesh;
+            udpated = true;
+        }
+        //_baseMesh.name = baseMeshName;
+        //}
+
+        string modifiedMeshName = gameObject.name + "_modifiedmesh";
+        if (_modifiedMesh == null || _modifiedMesh.name != modifiedMeshName)
+        {
+            _modifiedMesh = Instantiate(_baseMesh);
+            _modifiedMesh.name = modifiedMeshName;
+            if (_canBeModified)
+            {
+                ApplyHelixModifier();
+            }
+            udpated = true;
+        }
+
+        if (udpated)
+        {
+            _meshFilter.mesh = _canBeModified ? _modifiedMesh : _baseMesh;
+        }
+
+        if (_nodes == null || _nodes.Count == 0) return;
+        for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+        {
+            if (_nodes[nodeIndex] == null) continue;
+            _nodes[nodeIndex].UpdateMeshes();
+        }
+    }
+
+    private void RevertHelixModifier()
+    {
+        if (_baseMesh != null)
+        {
+            _meshFilter.mesh = _baseMesh;
+        }
+
+        if (_nodes == null || _nodes.Count == 0) return;
+        for (int nodeIndex = 0; nodeIndex < _nodes.Count; nodeIndex++)
+        {
+            if (_nodes[nodeIndex] == null) continue;
+            Vector3 modifiedPosition = _nodes[nodeIndex].gameObject.transform.localPosition;
+            Vector3 resetPosition = new Vector3(0f, modifiedPosition.y, 0f);
+            _nodes[nodeIndex].gameObject.transform.localPosition = resetPosition;
+        }
+    }
+
+    public void GenerateUV_LocalYMask(int channel, in Vector3[] curveCenters)
+    {
+        if (_modifiedMesh == null || _modifier == null || curveCenters.Length == 0) return;
+
+        Vector3[] vertices = _modifiedMesh.vertices;
+        if (vertices == null || vertices.Length == 0) return;
+
+        Vector3 scale = Vector3.one;
+        float yPosition = 0f;
+        Transform transform = gameObject.transform;
+        while (transform != transform.root)
+        {
+            PlantNode currentNode = transform.GetComponent<PlantNode>();
+            bool currentUsesRadialGrowth = currentNode && currentNode._useRadialGrowth;
+
+            PlantNode parentNode = transform.parent.GetComponent<PlantNode>();
+            bool parentUsesRadialGrowth = parentNode && parentNode._useRadialGrowth;
+
+            if (parentUsesRadialGrowth || currentUsesRadialGrowth)
+            {
+                yPosition += transform.localPosition.y * transform.parent.localScale.y;
+            }
+
+            scale.x *= transform.localScale.x;
+            scale.y *= transform.localScale.y;
+            scale.z *= transform.localScale.z;
+
+            transform = transform.parent;
+        }
+
+        Vector3 toOriginWS = Vector3.zero;
+        if (!_useRadialGrowth)
+        {
+            transform = gameObject.transform;
+            if (transform.parent != transform.root)
+            {
+                //toOriginWS = gameObject.transform.position;
+                while (transform != transform.root)
+                {
+                    PlantNode parentNode = transform.parent.GetComponent<PlantNode>();
+                    bool parentUsesRadialGrowth = parentNode && parentNode._useRadialGrowth;
+
+                    if (parentUsesRadialGrowth)
+                    {
+                        break;
+                    }
+                    toOriginWS = gameObject.transform.position - transform.parent.position;
+
+                    transform = transform.parent;
+                }
+            }
+        }
+
+        Vector4[] uv = new Vector4[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 offset = vertices[i];
+            if (_useRadialGrowth)
+            {
+                offset -= curveCenters[i];
+            }
+
+            offset.x *= scale.x;
+            offset.y *= scale.y;
+            offset.z *= scale.z;
+            offset = gameObject.transform.rotation * offset;
+            offset += toOriginWS;
+
+            float shiftedPosition = yPosition; // + vertices[i].y * gameObject.transform.localScale.y;
+            if (_useRadialGrowth)
+            {
+                shiftedPosition += (vertices[i].y * gameObject.transform.localScale.y);
+            }
+
+            //shiftedPosition += _useRadialGrowth ? vertices[i].y * scale.y : 0f;
+            uv[i] = new Vector4(offset.x, offset.y, offset.z, shiftedPosition);
+        }
+
+        _modifiedMesh.SetUVs(channel, uv);
+        EditorUtility.SetDirty(_modifiedMesh);
+    }
+
+    private void GenerateUV_LocalXY(int channel)
+    {
+        if (_modifiedMesh == null || _modifier == null) return;
+
+        Vector3[] vertices = _modifiedMesh.vertices;
+        if (vertices == null || vertices.Length == 0) return;
+
+        Transform transform = gameObject.transform;
+        Vector3 nodalOriginWS = Vector3.zero;
+        while (transform != transform.root)
+        {
+            nodalOriginWS = transform.position;
+            transform = transform.parent;
+        }
+
+        Vector4[] uv = new Vector4[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uv[i] = new Vector4(nodalOriginWS.x, nodalOriginWS.z, 0f, 0f);
+        }
+
+        _modifiedMesh.SetUVs(channel, uv);
+        EditorUtility.SetDirty(_modifiedMesh);
+    }
+    private void OnDestroy()
+    {
+        if (_modifiedMesh == null) return;
+
+        if (Application.isEditor)
+        {
+            DestroyImmediate(_modifiedMesh);
+        }
+        else
+        {
+            Destroy(_modifiedMesh);
+        }
+    }
+}
+}
